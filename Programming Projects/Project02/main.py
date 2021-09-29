d2df136a55bd0f80493d80a051d5c7c1ad764213
aguilaraul <raula@live.com>
2021-09-29 00:29:30 -0700

    Project 02

---
+++ Programming Projects/Project02/main.py
@@ -0,0 +1,39 @@
+'''
+DEVELOPER: Raul Aguilar
+COLLABORATORS: <other names>
+COURSE:	CS226 Discrete Structures
+PROJECT:	Project01
+LAST MODIFIED: 09 23 2021
+'''
+##########################################
+#	<TITLE OF PROGRAM>
+########################################## PROGRAM DESCRIPTION:
+# Write a project that takes as input two sets, A and B, and outputs the following:
+# AuB, AnB, AxB, AxorB, 
+# The program should take file input and output to both the console and a file
+##########################################
+# IMPORTS:
+#   <list modules needed for program and their purpose>
+##########################################
+#<imports here>
+
+##########################################
+# FUNCTIONS:
+##########################################
+#DESCRIPTION:<description of what function does>
+#<define functions here>
+
+##########################################
+# MAIN PROGRAM:
+##########################################
+def main():
+    sets = []
+    i = 0
+    with open('input.txt') as f:
+        data = f.read()
+
+
+    print(data)
+
+
+main()
